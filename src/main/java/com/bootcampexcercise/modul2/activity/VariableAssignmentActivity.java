d6a55349ac201e809ba57010f180563c5ba96b45
oleksandr_shu <shushin2000@gmail.com>
2026-04-07 21:20:44 +0300

    create VariableAssignmentActivity class

---
+++ src/main/java/com/bootcampexcercise/modul2/activity/VariableAssignmentActivity.java
@@ -0,0 +1,12 @@
+package com.bootcampexcercise.modul2.activity;
+
+public class VariableAssignmentActivity {
+    public static void main() {
+        String name = "Alex";
+        int age = 21;
+        boolean isProgrammer = true;
+
+        age += 1;
+        System.out.println("Hello " + name + ", your age will be " + age);
+    }
+}
