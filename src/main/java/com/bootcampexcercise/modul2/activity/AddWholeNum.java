c1ba867726b9ff788ea192bfc83f012e0a06edc2
oleksandr_shu <shushin2000@gmail.com>
2026-04-07 21:00:18 +0300

    created AddWholeNum class

--- src/main/java/com/bootcampexcercise/modul2/activity/AddWholeNum.java
+++ src/main/java/com/bootcampexcercise/modul2/activity/AddWholeNum.java
@@ -1,4 +1,23 @@
 package com.bootcampexcercise.modul2.activity;
-
+// Oleksandr Shushyn
 public class AddWholeNum {
+    public static void main(){
+
+        int result = 0;
+
+        for (int i = 50; i <= 100; i++) {
+            result += i;
+        }
+        System.out.println("Sum = " + result);
+        System.out.println("Sum: " + sumOfInt(50,100));
+
+
+    }
+    public static int sumOfInt(int a, int b) {
+        int sum = 0;
+        for (int i = a; i <= b; i++){
+            sum += i;
+        }
+        return sum;
+    }
 }
