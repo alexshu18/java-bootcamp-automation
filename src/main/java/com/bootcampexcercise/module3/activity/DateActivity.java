7a35a98be4d4aff33f84958a49739b39956f9671
oleksandr_shu <shushin2000@gmail.com>
2026-04-08 18:28:38 +0300

    create DateActivity class

---
+++ src/main/java/com/bootcampexcercise/module3/activity/DateActivity.java
@@ -0,0 +1,9 @@
+package com.bootcampexcercise.module3.activity;
+import java.util.Date;
+public class DateActivity {
+    public static void main(String[] args){
+        Date d = new Date();
+        System.out.println(d);
+    }
+
+}
