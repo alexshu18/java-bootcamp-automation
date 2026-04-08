fea0a78c8ca613cf1ed0c3a30ce6dd8744bd8432
oleksandr_shu <shushin2000@gmail.com>
2026-04-08 10:10:16 +0300

    adding comments to MultiplicationTable class

--- src/main/java/com/bootcampexcercise/modul2/activity/MultiplicationTable.java
+++ src/main/java/com/bootcampexcercise/modul2/activity/MultiplicationTable.java
@@ -2,9 +2,9 @@ package com.bootcampexcercise.modul2.activity;
 
 public class MultiplicationTable {
     public static void main() {
-        for (int i = 11; i <= 20; i++){
-            for (int j = 1; j <= 10; j++) {
-                System.out.println(i + " * " + j + " = " + i * j);
+        for (int i = 11; i <= 20; i++){ //for loop for numbers to multiply
+            for (int j = 1; j <= 10; j++) { // second loop to have 1 - 10 multipliers
+                System.out.println(i + " * " + j + " = " + i * j);//printing the result in normal view
             }
 
         }
