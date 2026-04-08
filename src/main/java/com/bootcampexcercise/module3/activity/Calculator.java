b287e08a5057b98f8b8ac7dd7883d5b3644c3a2e
oleksandr_shu <shushin2000@gmail.com>
2026-04-08 19:02:12 +0300

    create Calculator new and CalculatorClient classes

---
+++ src/main/java/com/bootcampexcercise/module3/activity/Calculator.java
@@ -0,0 +1,32 @@
+package com.bootcampexcercise.module3.activity;
+
+public class Calculator {
+    public static final String CALC_TYPE_BASIC = "basic";
+
+    private String calculatorType;
+
+    public Calculator(String calculatorType){
+        this.calculatorType = calculatorType;
+    }
+
+    public String getCalculatorType(){
+        return calculatorType;
+    }
+
+    public int add(int a, int b){
+        return a + b;
+    }
+    public int subtract(int a, int b){
+        return a - b;
+    }
+    public int divide(int a, int b){
+        return a / b;
+    }
+    public int multiply(int[] intValues){
+        int result = 1;
+        for (int i = 0; i < intValues.length; i++){
+            result *= intValues[i];
+        }
+        return result;
+    }
+}
