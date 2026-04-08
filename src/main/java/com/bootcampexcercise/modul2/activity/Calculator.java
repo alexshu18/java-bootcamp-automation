9df86d739a6128d8e95dfc4ba81e5d619c01c095
oleksandr_shu <shushin2000@gmail.com>
2026-04-08 10:30:15 +0300

    creating Calculator class

---
+++ src/main/java/com/bootcampexcercise/modul2/activity/Calculator.java
@@ -0,0 +1,19 @@
+package com.bootcampexcercise.modul2.activity;
+
+public class Calculator {
+    public static double add(double a, double b){
+        return a + b;
+    }
+    public static double subtract(double a, double b){
+        return a - b;
+    }
+    public static double multiply(double a, double b){
+        return a * b;
+    }
+    public static double modulo(double a, double b){
+        return a % b;
+    }
+    public static int division(int a, int b){
+        return a / b;
+    }
+}
