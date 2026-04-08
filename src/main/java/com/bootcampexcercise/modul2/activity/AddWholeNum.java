bde989c4b7651a3c866d261c634334d77dbcea43
oleksandr_shu <shushin2000@gmail.com>
2026-04-08 10:08:28 +0300

    adding some comments to AddWholeNum class

--- src/main/java/com/bootcampexcercise/modul2/activity/AddWholeNum.java
+++ src/main/java/com/bootcampexcercise/modul2/activity/AddWholeNum.java
@@ -3,19 +3,19 @@ package com.bootcampexcercise.modul2.activity;
 public class AddWholeNum {
     public static void main(){
 
-        int result = 0;
+        int result = 0; // declaring the variable
 
-        for (int i = 50; i <= 100; i++) {
+        for (int i = 50; i <= 100; i++) {//basic approach to have the result of sum of numbers from 50 to 100
             result += i;
         }
-        System.out.println("Sum = " + result);
-        System.out.println("Sum: " + sumOfInt(50,100));
+        System.out.println("Sum = " + result);//printing
+        System.out.println("Sum: " + sumOfInt(50,100));//calling this method and printing
 
 
     }
-    public static int sumOfInt(int a, int b) {
-        int sum = 0;
-        for (int i = a; i <= b; i++){
+    public static int sumOfInt(int a, int b) {//creating method with parameters for flexibility
+        int sum = 0; //declaring sum
+        for (int i = a; i <= b; i++){ //for loop for sum
             sum += i;
         }
         return sum;
