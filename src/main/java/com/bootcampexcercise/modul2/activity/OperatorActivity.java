959fc89176e858ba7a363c90357246dd13a70b56
oleksandr_shu <shushin2000@gmail.com>
2026-04-08 10:16:32 +0300

    adding comments to OperatorActivity class

--- src/main/java/com/bootcampexcercise/modul2/activity/OperatorActivity.java
+++ src/main/java/com/bootcampexcercise/modul2/activity/OperatorActivity.java
@@ -2,16 +2,16 @@ package com.bootcampexcercise.modul2.activity;
 //Oleksandr Shushyn
 public class OperatorActivity {
     public static void main() {
-        int x = 10, y = 20, z = 30;
-        int sum = x + y + z;
-        int product = x * y * z;
+        int x = 10, y = 20, z = 30; // declaring variable
+        int sum = x + y + z;//testing sum
+        int product = x * y * z;//testing product
 
-        System.out.println("Sum: " + sum);
-        System.out.println("Product: " + product);
+        System.out.println("Sum: " + sum);//printing sum
+        System.out.println("Product: " + product);//printing product
 
-        int a = z % y;
+        int a = z % y;//test other operator
         System.out.println(a);
-        int b = (x + y) * z;
+        int b = (x + y) * z;//adding brackets
         int c = x + y * z;
         System.out.println(b);
         System.out.println(c);
