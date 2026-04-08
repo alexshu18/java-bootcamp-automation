b069d6584e7012cd179c66582685f6cee88faab3
oleksandr_shu <shushin2000@gmail.com>
2026-04-08 09:24:32 +0300

    create MultiplicationTable class

---
+++ src/main/java/com/bootcampexcercise/modul2/activity/MultiplicationTable.java
@@ -0,0 +1,12 @@
+package com.bootcampexcercise.modul2.activity;
+
+public class MultiplicationTable {
+    public static void main() {
+        for (int i = 11; i <= 20; i++){
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " * " + j + " = " + i * j);
+            }
+
+        }
+    }
+}
