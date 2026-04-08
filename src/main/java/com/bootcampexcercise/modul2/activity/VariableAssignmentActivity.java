98db218d662564a3b3c41ff9b56677b3a9d03e9a
oleksandr_shu <shushin2000@gmail.com>
2026-04-08 10:17:44 +0300

    adding comments to VariableAssignmentActivity class

--- src/main/java/com/bootcampexcercise/modul2/activity/VariableAssignmentActivity.java
+++ src/main/java/com/bootcampexcercise/modul2/activity/VariableAssignmentActivity.java
@@ -2,11 +2,11 @@ package com.bootcampexcercise.modul2.activity;
 
 public class VariableAssignmentActivity {
     public static void main() {
-        String name = "Alex";
-        int age = 21;
-        boolean isProgrammer = true;
+        String name = "Alex";//declare string
+        int age = 21;//declare int
+        boolean isProgrammer = true;//declare boolean
 
-        age += 1;
-        System.out.println("Hello " + name + ", your age will be " + age);
+        age += 1; // add 1 to age variable
+        System.out.println("Hello " + name + ", your age will be " + age);//print the result
     }
 }
