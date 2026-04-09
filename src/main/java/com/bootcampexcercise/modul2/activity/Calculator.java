848b22bdb2136b39da4ad34500ec893b94400382
oleksandr_shu <shushin2000@gmail.com>
2026-04-09 15:02:34 +0300

    correct Issue1 according to Calculator class: Add main method inside, and delete call in Main class

--- src/main/java/com/bootcampexcercise/modul2/activity/Calculator.java
+++ src/main/java/com/bootcampexcercise/modul2/activity/Calculator.java
@@ -1,6 +1,13 @@
 package com.bootcampexcercise.modul2.activity;
 //Oleksandr Shushym
 public class Calculator {
+    static void main(String[] args) {
+        System.out.println(Calculator.add(5,3));
+        System.out.println(Calculator.subtract(10,7));
+        System.out.println(Calculator.multiply(2,3));
+        System.out.println(Calculator.modulo(5,2));
+        System.out.println(Calculator.division(10,2));
+    }
     public static double add(double a, double b){
         return a + b;
     }
--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -6,10 +6,6 @@ import com.bootcampexcercise.modul2.activity.Calculator;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main(String[] args) {
-        System.out.println(Calculator.add(5,3));
-        System.out.println(Calculator.subtract(10,7));
-        System.out.println(Calculator.multiply(2,3));
-        System.out.println(Calculator.modulo(5,2));
-        System.out.println(Calculator.division(10,2));
+
     }
 }
