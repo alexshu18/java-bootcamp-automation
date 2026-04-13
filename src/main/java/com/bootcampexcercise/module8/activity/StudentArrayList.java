26482dd46113859b9d2fd04cded4438388d1d028
oleksandr_shu <shushin2000@gmail.com>
2026-04-13 10:57:55 +0300

    corrected methods in StudentArrayList and created StudentTest class

--- src/main/java/com/bootcampexcercise/module8/activity/StudentArrayList.java
+++ src/main/java/com/bootcampexcercise/module8/activity/StudentArrayList.java
@@ -7,31 +7,23 @@ public class StudentArrayList {
     public static void main(String[] args) {
         ArrayList<Student> students = new ArrayList<>();
 
-        students.add(new Student(1, "Alex", 5));
-        students.add(new Student(2, "Vlad", 4));
-        students.add(new Student(3, "Gleb", 3));
-        students.add(new Student(4, "Ilya", 2));
+        students.add(new Student(1, "Alex", 5.0));
+        students.add(new Student(2, "Vlad", 4.0));
+        students.add(new Student(3, "Gleb", 3.0));
+        students.add(new Student(4, "Ilya", 2.0));
         StudentArrayList.addStudent(students, new Student(2, "Dima", 4.5));
 
 
         StudentArrayList.printAllStudents(students);
         System.out.println("All students.");
         System.out.println("---------");
-        for (int i = 0; i < students.size(); i++) {
-            if (students.get(i).getId() == 3) {
-                students.remove(students.get(i));
-                break;
-            }
-        }
+
         StudentArrayList.printAllStudents(students);
         System.out.println("---------");
-        Student topStudent = students.get(0);
-        for (Student student : students) {
-            if (student.getGrade() > topStudent.getGrade()) {
-                topStudent = student;
-            }
-        }
-        topStudent.printStudent();
+        Student top = topStudent(students);
+        top.printStudent();
+        System.out.println("---------");
+        deleteStudent(students, 1);
         }
         public static void addStudent(ArrayList<Student> students, Student student){
             for (Student s : students) {
@@ -46,6 +38,26 @@ public class StudentArrayList {
             for (Student student : students) {
                 student.printStudent();
             }
+        }
+
+        public static Student topStudent(ArrayList<Student> students){
+            Student topStudent = students.get(0);
+            for (Student student : students) {
+                if (student.getGrade() > topStudent.getGrade()) {
+                    topStudent = student;
+                }
+            }
+            return topStudent;
+        }
+
+        public static void deleteStudent(ArrayList<Student> students, int id){
+            for (int i = 0; i < students.size(); i++) {
+                if (students.get(i).getId() == id) {
+                    students.remove(students.get(i));
+                    break;
+                }
+            }
+            printAllStudents(students);
         }
     }
 
--- src/test/java/com/bootcampexcercise/module9/activity/StudentTest.java
+++ src/test/java/com/bootcampexcercise/module9/activity/StudentTest.java
@@ -1,4 +1,46 @@
 package com.bootcampexcercise.module9.activity;
+import com.bootcampexcercise.module8.activity.StudentArrayList;
+import com.bootcampexcercise.module8.activity.Student;
+import junit.framework.TestCase;
 
-public class StudentTest {
+import java.util.ArrayList;
+//Oleksandr Shushyn
+
+public class StudentTest extends TestCase {
+    private ArrayList<Student> student;
+
+    @Override protected void setUp(){
+        student = new ArrayList<Student>();
+        student.add(new Student(1, "Alex", 4));
+        student.add(new Student(2, "Dima", 3));
+        student.add(new Student(3, "Vlad", 5));
+    }
+
+    @Override protected void tearDown(){
+        student = null;
+    }
+
+    public void testAddStudent(){
+        StudentArrayList.addStudent(student, new Student(4, "Ilya", 2));
+        assertEquals(4, student.size());
+    }
+
+    public void testAddSameStudent(){
+        StudentArrayList.addStudent(student, new Student(1, "Alex", 4));
+        assertEquals(3, student.size());
+    }
+
+    public void testDeleteStudent(){
+        StudentArrayList.deleteStudent(student, 1);
+        assertEquals("Dima", student.getFirst().getName());
+    }
+
+    public void testTopStudent(){
+        Student top = StudentArrayList.topStudent(student);
+        assertEquals(5.0, top.getGrade());
+    }
+
+    public void testSetUp(){
+        assertEquals(3, student.size());
+    }
 }
